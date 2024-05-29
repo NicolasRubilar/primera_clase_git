df08be9d4567c0ad791c07059cd0c5978e51b102
nicolashernandez <nico.hernandezr@duocuc.cl>
2024-05-29 10:51:58 -0400

    principal_menu

---
+++ ejercicio1_29_05.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+print("Menu")
+print("1) Iniciar sesión")
+print("2) Registrar usuario")
+print("3) Eliminar usuario")
+print("4) Salir")
+
+opc=int(input("Ingrese Opcion: "))
+if opc==1:
+    pass
+elif opc==2:
+    pass
+elif opc==3:
+    pass
+else:
+    print("hasta luego!")
+    break
